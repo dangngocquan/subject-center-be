cec734af017df46b963112c8a333b7d057555bba
admin <dangngocquan104@gmail.com>
2025-03-08 11:45:12 +0700

    fix: vercel swagger static

--- src/main.ts
+++ src/main.ts
@@ -2,71 +2,56 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config as dotenvConfig } from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { get } from 'http';
 import { createWriteStream } from 'fs';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-dotenvConfig({ path: '.env' });
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+function setGlobalPrefix(app: INestApplication<any>, logger: Logger) {
+  logger.debug(`[setGlobalPrefix] Start set global prefix ...`);
   app.setGlobalPrefix('api', {
-    exclude: [''],
+    exclude: ['/'],
   });
+  logger.debug(`[setGlobalPrefix] Finish set global prefix.`);
+}
 
-  // CORS
-  // const allowedOrigins = `${process.env.ALLOW_ORIGIN}`.split('|');
-  // const corsOptions: CorsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (!origin || allowedOrigins.includes(origin)) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  //   preflightContinue: false, // Ensure this is set to false to handle preflight automatically
-  //   optionsSuccessStatus: 204,
-  // };
-  // app.enableCors(corsOptions);
+function setCors(app: INestApplication<any>, logger: Logger) {
+  logger.debug(`[setCors] Start set cors...`);
+  app.enableCors({
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, X-Telegram-Uid, Content-Type, Accept, Observe, Token, Lang, Token-Secret, Api-Key-User, Api-Key-Admin',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  logger.debug(`[setCors] Finish set cors.`);
+}
 
-  // SWAGGER
-  const config = new DocumentBuilder()
+function setSwagger(app: INestApplication<any>, logger: Logger) {
+  logger.debug(`[setSwagger] Start set swagger ...`);
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('API description')
     .setVersion('1.0')
-    .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, document);
+  logger.debug(`[setSwagger] Finish set swagger.`);
+}
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      stopAtFirstError: true,
-    }),
-  );
-
-  const port = process.env.APP_PORT;
-  await app.listen(port);
-  console.info(`Running on port ${port}`);
-  console.info(
-    `Documentation: http://localhost:${port}/docs`,
-  );
-
+async function setStaticSwagger(app: INestApplication<any>, logger: Logger) {
   // get the swagger json file (if app is running in development mode)
   if (process.env.NODE_ENV === 'development') {
     const serverUrl = await app.getUrl();
     // write swagger ui files
-    get(`${serverUrl}/documentation/swagger-ui-bundle.js`, function (response) {
+    get(`${serverUrl}/docs/swagger-ui-bundle.js`, function (response) {
       response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
       console.log(
         `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
       );
     });
 
-    get(`${serverUrl}/documentation/swagger-ui-init.js`, function (response) {
+    get(`${serverUrl}/docs/swagger-ui-init.js`, function (response) {
       response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
       console.log(
         `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
@@ -74,7 +59,7 @@ async function bootstrap() {
     });
 
     get(
-      `${serverUrl}/documentation/swagger-ui-standalone-preset.js`,
+      `${serverUrl}/docs/swagger-ui-standalone-preset.js`,
       function (response) {
         response.pipe(
           createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
@@ -85,7 +70,7 @@ async function bootstrap() {
       },
     );
 
-    get(`${serverUrl}/documentation/swagger-ui.css`, function (response) {
+    get(`${serverUrl}/docs/swagger-ui.css`, function (response) {
       response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
       console.log(
         `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
@@ -93,4 +78,110 @@ async function bootstrap() {
     });
   }
 }
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    bufferLogs: true,
+    rawBody: true,
+  });
+  const logger = new Logger('MAIN');
+
+  setGlobalPrefix(app, logger);
+  setCors(app, logger);
+  setSwagger(app, logger);
+
+  const port = process.env.APP_PORT;
+  await app.listen(port);
+
+  logger.log(`[PROCESS] ${process.env.PROCESS}`);
+
+  logger.log(`[PORT] Running on port ${port}`);
+  logger.log(`[DOCS] Documentation: http://localhost:${port}/docs`);
+
+  await setStaticSwagger(app, logger);
+}
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.setGlobalPrefix('api', {
+//     exclude: [''],
+//   });
+
+//   // CORS
+//   // const allowedOrigins = `${process.env.ALLOW_ORIGIN}`.split('|');
+//   // const corsOptions: CorsOptions = {
+//   //   origin: function (origin, callback) {
+//   //     if (!origin || allowedOrigins.includes(origin)) {
+//   //       callback(null, true);
+//   //     } else {
+//   //       callback(new Error('Not allowed by CORS'));
+//   //     }
+//   //   },
+//   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//   //   credentials: true,
+//   //   preflightContinue: false, // Ensure this is set to false to handle preflight automatically
+//   //   optionsSuccessStatus: 204,
+//   // };
+//   // app.enableCors(corsOptions);
+
+//   // SWAGGER
+//   const config = new DocumentBuilder()
+//     .setTitle('API Documentation')
+//     .setDescription('API description')
+//     .setVersion('1.0')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('docs', app, document);
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       stopAtFirstError: true,
+//     }),
+//   );
+
+//   const port = process.env.APP_PORT;
+//   await app.listen(port);
+//   console.info(`Running on port ${port}`);
+//   console.info(`Documentation: http://localhost:${port}/docs`);
+
+//   // get the swagger json file (if app is running in development mode)
+//   if (process.env.NODE_ENV === 'development') {
+//     const serverUrl = await app.getUrl();
+//     // write swagger ui files
+//     get(`${serverUrl}/docs/swagger-ui-bundle.js`, function (response) {
+//       response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+//       console.log(
+//         `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+//       );
+//     });
+
+//     get(`${serverUrl}/docs/swagger-ui-init.js`, function (response) {
+//       response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+//       console.log(
+//         `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+//       );
+//     });
+
+//     get(
+//       `${serverUrl}/docs/swagger-ui-standalone-preset.js`,
+//       function (response) {
+//         response.pipe(
+//           createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+//         );
+//         console.log(
+//           `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+//         );
+//       },
+//     );
+
+//     get(`${serverUrl}/docs/swagger-ui.css`, function (response) {
+//       response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+//       console.log(
+//         `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+//       );
+//     });
+//   }
+// }
 bootstrap();
--- swagger-static/swagger-ui-init.js
+++ swagger-static/swagger-ui-init.js
@@ -1,1 +1,76 @@
-{"message":"Cannot GET /documentation/swagger-ui-init.js","error":"Not Found","statusCode":404}
+
+window.onload = function() {
+  // Build a system
+  let url = window.location.search.match(/url=([^&]+)/);
+  if (url && url.length > 1) {
+    url = decodeURIComponent(url[1]);
+  } else {
+    url = window.location.origin;
+  }
+  let options = {
+  "swaggerDoc": {
+    "openapi": "3.0.0",
+    "paths": {
+      "/": {
+        "get": {
+          "operationId": "AppController_getHello",
+          "parameters": [],
+          "responses": {
+            "200": {
+              "description": ""
+            }
+          },
+          "tags": [
+            "App"
+          ]
+        }
+      }
+    },
+    "info": {
+      "title": "API Documentation",
+      "description": "API description",
+      "version": "1.0",
+      "contact": {}
+    },
+    "tags": [],
+    "servers": [],
+    "components": {
+      "schemas": {}
+    }
+  },
+  "customOptions": {}
+};
+  url = options.swaggerUrl || url
+  let urls = options.swaggerUrls
+  let customOptions = options.customOptions
+  let spec1 = options.swaggerDoc
+  let swaggerOptions = {
+    spec: spec1,
+    url: url,
+    urls: urls,
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  }
+  for (let attrname in customOptions) {
+    swaggerOptions[attrname] = customOptions[attrname];
+  }
+  let ui = SwaggerUIBundle(swaggerOptions)
+
+  if (customOptions.initOAuth) {
+    ui.initOAuth(customOptions.initOAuth)
+  }
+
+  if (customOptions.authAction) {
+    ui.authActions.authorize(customOptions.authAction)
+  }
+  
+  window.ui = ui
+}
